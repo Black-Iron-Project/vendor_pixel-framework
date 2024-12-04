RisingTechOSS/android_vendor_pixel-framework

SettingsGoogle: Show extreme battery saver only if flipendo available
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

SettingsGoogle/src/com/google/android/settings/fuelgauge/batterysaver/BatterySaverModePreferenceController.java
@@ -14,6 +14,7 @@ import android.view.View;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.crdroid.Utils;
 import com.android.settings.core.BasePreferenceController;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnPause;
@@ -37,7 +38,8 @@ public class BatterySaverModePreferenceController extends BasePreferenceControll
 
     @Override 
     public int getAvailabilityStatus() {
-        return 0;
+        boolean available = Utils.isPackageInstalled(mContext, "com.google.android.flipendo", false);
+        return available ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override 

Black-Iron-Project/vendor_pixel-framework

SettingsGoogle/src/com/google/android/settings/fuelgauge/batterysaver/BatterySaverModePreferenceController.java
@@ -14,6 +14,7 @@ import android.view.View;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.blackiron.Utils;
 import com.android.settings.core.BasePreferenceController;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnPause;
@@ -37,7 +38,8 @@ public class BatterySaverModePreferenceController extends BasePreferenceControll
 
     @Override 
     public int getAvailabilityStatus() {
-        return 0;
+        boolean available = Utils.isPackageInstalled(mContext, "com.google.android.flipendo", false);
+        return available ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override 